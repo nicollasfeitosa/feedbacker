5cec5db5c238bb82317d386ea638172d402e0be3
nicollasdev <contatonicollasfeitosa@gmail.com>
2021-08-21 16:32:26 -0300

    Initial commit

---
+++ feedbacker-frontend/src/services/index.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import AuthService from './auth'
+import router from '../router'
+import UserServices from './users'
+import { setGlobalLoading } from '../store/global'
+
+const API_ENVS = {
+  production: '',
+  development: '',
+  local: 'http://localhost:3000'
+}
+
+const httpClient = axios.create({
+  baseURL: API_ENVS.local
+})
+
+httpClient.interceptors.request.use(config => {
+  setGlobalLoading(true)
+  const token = window.localStorage.getItem('token')
+
+  if (token) {
+    config.headers.common.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
+httpClient.interceptors.response.use((response) => {
+  setGlobalLoading(false)
+  return response
+}, (error) => {
+  const canThrowAnError = error.request.status === 0 ||
+  error.request.status === 500
+
+  if (canThrowAnError) {
+    setGlobalLoading(false)
+    throw new Error(error.message)
+  }
+
+  if (error.response.status === 401) {
+    router.push({ name: 'Home' })
+  }
+
+  setGlobalLoading(false)
+  return error
+})
+
+export default {
+  auth: AuthService(httpClient),
+  users: UserServices(httpClient)
+}
---
+++ feedbacker-frontend/src/views/Credencials/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <header-logged />
+  </div>
+
+  <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">
+      Credenciais
+    </h1>
+    <p class="text-lg text-center text-gray-800 font-regular">
+      Guia de instalação e geração de suas credenciais
+    </p>
+  </div>
+
+  <div class="flex justify-center w-full h-full">
+    <div class="flex flex-col w-4/5 max-w-6xl py-10">
+      <h1 class="text-3xl font-black text-brand-darkgray">
+        Instalação e configuração
+      </h1>
+      <p class="mt-10 text-lg text-gray-800 font-regular">
+        Essa aqui é a sua chave de api
+      </p>
+
+      <div
+        class="flex py-3 pl-5 mt-2 rounded items-center bg-brand-gray w-1/2"
+      >
+        <span>{{ store.User.currentUser.apiKey }}</span>
+        <div class="flex ml-20 mr-1">
+          <icon
+            name="Copy"
+            :color="brandColors.graydark"
+            size="24"
+            class="cursor-pointer"
+          />
+          <icon
+            name="Loading"
+            :color="brandColors.graydark"
+            size="24"
+            class="cursor-pointer ml-3"
+          />
+        </div>
+      </div>
+
+      <p class="mt-5 text-lg text-gray-800 font-regular">
+        Coloque esse script abaixo no seu site para começar a receber feedbacks
+      </p>
+
+      <div
+        class="py-3 pl-5 pr-20 mt-5 rounded bg-brand-gray w-2/3 overflow-x-scroll"
+      >
+        <pre>&lt;script src="https://nicollasfeitosa-feedbacker-widget.netlify.app?apiKey={{ store.User.currentUser.apiKey }}"&gt;&lt;/script&gt;</pre>
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+import HeaderLogged from '../../components/HeaderLogged'
+import Icon from '../../components/Icon'
+import useStore from '../../hooks/useStore'
+import palette from '../../../palette.js'
+
+export default {
+  components: { HeaderLogged, Icon },
+  setup () {
+    const store = useStore()
+
+    return {
+      store,
+      brandColors: palette.brand
+    }
+  }
+}
+</script>
